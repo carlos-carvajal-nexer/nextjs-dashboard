24282673b46c2f580b5849c24e7b7b0c2100759a
carlos-carvajal-nexer <carlos.carvajal@nexergroup.com>
2024-01-17 10:25:19 -0500

    First commit

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+import '@/app/ui/global.css';
+import { quickSand } from '@/app/ui/fonts';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${quickSand.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,3 @@
+import { Quicksand, Lusitana } from 'next/font/google';
+export const quickSand = Quicksand({ subsets: ['latin'] });
+export const lusitana = Lusitana({weight:['400', '700'], subsets: ['latin'] });
